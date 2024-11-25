ef8d2442a8878e354b44eb7f5262a1ebb9f93df7
JH2XX <158313400+JH2XX@users.noreply.github.com>
2024-11-25 17:46:38 -0500

    feat: implemented function for square root

--- main.py
+++ main.py
@@ -1,4 +1,38 @@
-def test_square_root():
+import math
+import random
+
+def square_root(num):
+    if num < 0:
+        raise ValueError("Cannot calculate square root of a negative number")
+    return math.sqrt(num)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def test_should_return_square_root():
     assert square_root(4) == 2
     assert square_root(16) == 4
     assert square_root(0) == 0
